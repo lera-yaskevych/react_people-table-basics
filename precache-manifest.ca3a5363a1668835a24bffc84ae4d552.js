8362ad2bb34aba21859021a951a9a4414155050a
lera-yaskevych <redrum.back@gmail.com>
2021-09-18 14:11:21 +0300

    Updates

---
+++ precache-manifest.ca3a5363a1668835a24bffc84ae4d552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e8005dec2767502adef8989ddf022b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6716a70953bfc197dcd",
+    "url": "./static/css/main.dd0f8124.chunk.css"
+  },
+  {
+    "revision": "7cd6fb5edc5534e6194f",
+    "url": "./static/js/2.0dfa3025.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0dfa3025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6716a70953bfc197dcd",
+    "url": "./static/js/main.d96514e7.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
